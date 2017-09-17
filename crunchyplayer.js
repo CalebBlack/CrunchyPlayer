f7c37d2a83a558a11e64ac09160e85b43228d0ac
CalebBlack <caleb@polished.tech>
2017-09-17 15:03:19 -0600

    Update crunchyplayer.js

--- crunchyplayer.js
+++ crunchyplayer.js
@@ -42,7 +42,7 @@ class CrunchyPlayer extends React.Component {
   }
   render(){
     return (
-      <div className='crunchyplayer'>
+      <div className={'crunchyplayer'+(this.props.className ? " "+this.props.className : "")}>
         <video autoPlay={this.props.autoplay || false} onClick={this.toggle} ref={(ref)=>{this.player = ref}}>
           <source src={this.props.source}/>
         </video>
